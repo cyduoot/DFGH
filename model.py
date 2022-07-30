cc7288d71976b7ba3db256f61cfdebce929600ff
cyduoot <cyduoot@hotmail.com>
2022-07-30 14:59:01 +0800

    update comments

--- model.py
+++ model.py
@@ -6,26 +6,20 @@ import torch.nn.functional as F
 
 
 class BasicModule(torch.nn.Module):
-    """
-    封装nn.Module，主要提供save和load两个方法
-    """
+
     def __init__(self):
         super(BasicModule, self).__init__()
         self.module_name = str(type(self))
 
     def load(self, path, use_gpu=False):
-        """
-        可加载指定路径的模型
-        """
+
         if not use_gpu:
             self.load_state_dict(torch.load(path, map_location=lambda storage, loc: storage))
         else:
             self.load_state_dict(torch.load(path))
 
     def save(self, name=None):
-        """
-        保存模型，默认使用"模型名字+时间"作为文件名
-        """
+
         if name is None:
             prefix = self.module_name + '_'
             name = time.strftime(prefix + '%m%d_%H:%M:%S.pth')
@@ -38,7 +32,7 @@ class BasicModule(torch.nn.Module):
 
 
 # vgg-f(cnn-f)
-# vgg-f结构：http://www.vlfeat.org/matconvnet/models/imagenet-vgg-f.svg
+# http://www.vlfeat.org/matconvnet/models/imagenet-vgg-f.svg
 class ImageNet(BasicModule):
     def __init__(self, bit, model_path, keep_prob, n_class):
         super(ImageNet, self).__init__()
@@ -51,23 +45,22 @@ class ImageNet(BasicModule):
         self.n_fc7 = 4096
         self.n_class = n_class
         self.mean = torch.from_numpy(self.data['meta'][0][0][2][0][0][2].transpose()).type(torch.float)
-        # conv1参数(nn.Parameter把参数加入模型的parameter列表中，实现可训练）
-        # matlab文件中的权重维度为（前两个维度：滤波器尺寸(高x宽)，维度3：输入通道，维度4：输出通道）
-        # 而pytorch中卷积层权重维度为输出通道 x 输入通道 x 卷积核高 x 卷积核宽
+        # conv1
+        # matlab file dim（h, w, in channel, out channel）
+        # pytorch dim (out channel, in channel, h, w)
         conv1_w, conv1_b = self.weights[0][0][0][2][0]
         conv1_b = conv1_b.reshape(-1)
         self.conv1_w = torch.nn.Parameter(torch.FloatTensor(conv1_w).permute(3, 2, 0, 1))
         self.conv1_b = torch.nn.Parameter(torch.FloatTensor(conv1_b))
         self.pad1 = self.weights[0][0][0][4][0]
-        # tensorflow中stride要求[1, stride_h, stride_w, 1]
         self.stride1 = self.weights[0][0][0][5][0]
 
-        # pool1参数
+        # pool1
         self.stride_pool1 = self.weights[3][0][0][4][0]
         self.pad_pool1 = self.weights[3][0][0][5][0]
         self.area_pool1 = self.weights[3][0][0][3][0]
 
-        # conv2参数
+        # conv2
         conv2_w, conv2_b = self.weights[4][0][0][2][0]
         conv2_b = conv2_b.reshape(-1)
         self.conv2_w = torch.nn.Parameter(torch.FloatTensor(conv2_w).permute(3, 2, 0, 1))
@@ -75,12 +68,12 @@ class ImageNet(BasicModule):
         self.pad2 = self.weights[4][0][0][4][0]
         self.stride2 = self.weights[4][0][0][5][0]
 
-        # pool2参数
+        # pool2
         self.stride_pool2 = self.weights[7][0][0][4][0]
         self.pad_pool2 = self.weights[7][0][0][5][0]
         self.area_pool2 = self.weights[7][0][0][3][0]
 
-        # conv3参数
+        # conv3
         conv3_w, conv3_b = self.weights[8][0][0][2][0]
         conv3_b = conv3_b.reshape(-1)
         self.conv3_w = torch.nn.Parameter(torch.FloatTensor(conv3_w).permute(3, 2, 0, 1))
@@ -88,7 +81,7 @@ class ImageNet(BasicModule):
         self.pad3 = self.weights[8][0][0][4][0]
         self.stride3 = self.weights[8][0][0][5][0]
 
-        # conv4参数
+        # conv4
         conv4_w, conv4_b = self.weights[10][0][0][2][0]
         conv4_b = conv4_b.reshape(-1)
         self.conv4_w = torch.nn.Parameter(torch.FloatTensor(conv4_w).permute(3, 2, 0, 1))
@@ -96,7 +89,7 @@ class ImageNet(BasicModule):
         self.pad4 = self.weights[10][0][0][4][0]
         self.stride4 = self.weights[10][0][0][5][0]
 
-        # conv5参数
+        # conv5
         conv5_w, conv5_b = self.weights[12][0][0][2][0]
         conv5_b = conv5_b.reshape(-1)
         self.conv5_w = torch.nn.Parameter(torch.FloatTensor(conv5_w).permute(3, 2, 0, 1))
@@ -104,18 +97,18 @@ class ImageNet(BasicModule):
         self.pad5 = self.weights[12][0][0][4][0]
         self.stride5 = self.weights[12][0][0][5][0]
 
-        # pool5参数
+        # pool5
         self.stride_pool5 = self.weights[14][0][0][4][0]
         self.pad_pool5 = self.weights[14][0][0][5][0]
         self.area_pool5 = self.weights[14][0][0][3][0]
 
-        # fc6参数
+        # fc6
         fc6_w, fc6_b = self.weights[15][0][0][2][0]
         fc6_b = fc6_b.reshape(-1)
         self.fc6_w = torch.nn.Parameter(torch.FloatTensor(fc6_w).permute(3, 2, 0, 1))
         self.fc6_b = torch.nn.Parameter(torch.FloatTensor(fc6_b))
 
-        # fc7参数
+        # fc7
         fc7_w, fc7_b = self.weights[17][0][0][2][0]
         fc7_b = fc7_b.reshape(-1)
         self.fc7_w = torch.nn.Parameter(torch.FloatTensor(fc7_w).permute(3, 2, 0, 1))
@@ -136,7 +129,7 @@ class ImageNet(BasicModule):
         else:
             x = x - self.mean
 
-        # pytorch输入图片维度：B x C x H x W，且pad方式为按维度逆序填充
+        # pytorch dim：B x C x H x W
         dim = [int(self.pad1[2]), int(self.pad1[3]), int(self.pad1[0]), int(self.pad1[1]), 0, 0, 0, 0]
         x = F.pad(x, dim, 'constant')
 
@@ -145,7 +138,7 @@ class ImageNet(BasicModule):
 
         x = F.local_response_norm(x, 2, alpha=0.0001, beta=0.75, k=2.000)
 
-        # 池化1
+        # pool1
         dim = [int(self.pad_pool1[2]), int(self.pad_pool1[3]), int(self.pad_pool1[0]), int(self.pad_pool1[1]), 0, 0, 0, 0]
         x = F.pad(x, dim, 'constant')
         x = F.max_pool2d(x, kernel_size=(int(self.area_pool1[0]), int(self.area_pool1[1])),
@@ -180,7 +173,7 @@ class ImageNet(BasicModule):
         x = F.max_pool2d(x, kernel_size=(int(self.area_pool5[0]), int(self.area_pool5[1])),
                          stride=(int(self.stride_pool5[0]), int(self.stride_pool5[1])))
 
-        # fc6和fc7
+        # fc6 and fc7
         x = F.conv2d(x, self.fc6_w, self.fc6_b, stride=1)
         x = F.relu(x)
         x = F.dropout(x, p=self.keep_prob)
@@ -230,7 +223,7 @@ class TextNet(BasicModule):
 
 
 if __name__ == '__main__':
-    model_path = '/home/qjq/pretrain_model/imagenet-vgg-f.mat'
+    model_path = 'pretrain_model/imagenet-vgg-f.mat'
     img_net = ImageNet(64, model_path, 0.5, 24)
 
 
